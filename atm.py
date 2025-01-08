58e6a51250a8ab441a063ccc1be93bb04353a2f3
정영하 <pd.hyazzang@gmail.com>
2025-01-08 18:05:34 +0900

    ✨ 입금 기능 추가
    사용자가 입금 금액을 입력하고, 잔액을 업데이트 하는 기능 추가, 영수증에 입금 기록을 저장합니다.

--- atm.py
+++ atm.py
@@ -2,9 +2,17 @@
 # 종료를 누르면 서비스를 종료한다는 메시지를 출력하고 현재 잔액을 보여주세요
 
 balance = 3000  #현재 잔액을 보여주세요
+receipts = [] #[]대괄호:list / {key:value}중괄호:dict / (): tuple
 
 while True:
     num = input("사용하실 기능을 선택해주세요 (1:입금, 2:출금, 3:영수증 보기, 4:종료)")
     if num == '4':
         break
+
+    elif num == '1':
+        deposit_amount = int(input("입금 금액을 입력하세요.: ")) #input()=내장함수 / int()=정수형 데이터로 형변환 해주는 내장함수
+        balance += deposit_amount #할당연산자 balance = balance + deposit_amount
+        receipts.append(("입금", deposit_amount, balance)) #.append : 리스트에 요소 집어넣기
+        print("입금하신 금액은 {deposit_amount}원이고, 현재 잔액은 {balance}원 입니다.")
+
 print(f"서비스를 종료합니다. 현재 잔액은 {balance}원 입니다.")
